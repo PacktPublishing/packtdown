5cc8365013bcce6b2555ac9fa39802d728d107b6
James Macmillan <jamescodesthings@gmail.com>
2019-01-24 08:46:02 +0000

    : ADD program configuration using commander

--- index.js
+++ index.js
@@ -3,8 +3,10 @@
 require('colors');
 
 const Debug = require('debug');
-const { version } = require('./package.json');
+const program = require('commander');
+const { version, description } = require('./package.json');
 
+// Set up Debug
 const log = Debug('packtdown:info');
 const error = Debug('packtdown:error');
 const debug = Debug('packtdown:debug');
@@ -12,5 +14,30 @@ const debug = Debug('packtdown:debug');
 log.enabled = true;
 error.enabled = true;
 
-log(`packtdown ${version}`.cyan);
+// log(`packtdown ${version}`.cyan);
 debug('Debugging enabled');
+
+// Set up the program
+program
+  .version(version, '-v, --version')
+  .description(description)
+  .option('-d, --directory <directory>', 'The directory to search for MD files')
+  .option('-g, --glob <glob>', 'A custom glob for MD files');
+
+program.parse(process.argv);
+
+let glob = './docs/**/*.md'; // default glob
+
+if (program.glob) {
+  // Set the glob
+  debug('Glob Set to', program.glob);
+
+  // eslint-disable-next-line prefer-destructuring
+  glob = program.glob;
+} else if (program.directory) {
+  debug('Directory set to', program.directory);
+  // Parse the directory into a glob
+  glob = `./${program.directory}/**/*.md`;
+}
+
+debug('Parsing files in', glob);
