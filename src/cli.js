3ea96ffd34d79591b23266136e3f5622288f1107
James Macmillan <jamescodesthings@gmail.com>
2019-01-29 11:39:02 +0000

    refactor: Moved to a proper program structure compatibility fixed at 8.10+ because older versions are almost out of support. cleaned up a lot.

--- src/cli.js
+++ src/cli.js
@@ -1,25 +1,8 @@
 // Packtdown
-require('colors');
-
-const Debug = require('debug');
 const program = require('commander');
-const glob = require('glob');
-const fs = require('fs-extra');
-const showdown = require('showdown');
-const watch = require('glob-watcher');
 
+const Packtdown = require('./packtdown.js');
 const { version, description } = require('../package.json');
-
-// Set up Debug
-const log = Debug('packtdown:info');
-const error = Debug('packtdown:error');
-const debug = Debug('packtdown:debug');
-// Force enable logging at info level for the CLI tool.
-log.enabled = true;
-error.enabled = true;
-
-// log(`packtdown ${version}`.cyan);
-debug('Debugging enabled');
 
 // Set up the program
 program
@@ -31,91 +14,9 @@ program
 
 program.parse(process.argv);
 
-let mdGlob = './docs/**/*.md'; // default glob
-
-if (program.glob) {
-  // Set the glob
-  debug('Glob Set to', program.glob);
-
-  // eslint-disable-next-line prefer-destructuring
-  mdGlob = program.glob;
-} else if (program.directory) {
-  debug('Directory set to', program.directory);
-  // Parse the directory into a glob
-  mdGlob = `./${program.directory}/**/*.md`;
-}
-
-debug('Parsing files in', mdGlob);
-
-// Run Showdown on each file in the Docs folder and output it
-// todo: directory to output? if default is no good.
-const promiseGlob = globToProcess =>
-  new Promise((res, rej) => {
-    glob(globToProcess, (err, files) => {
-      if (err) return rej(err);
-
-      return res(files);
-    });
-  });
-
-const processFile = file => {
-  // Spawn tagged logs because this is a fan-out.
-  const fileLog = Debug(file);
-  const fileLogDetailed = Debug(`${file}:DEBUG`);
-  fileLog.enabled = true;
-
-  fileLog('Started');
-
-  const converter = new showdown.Converter();
-  return fs.readFile(file).then(buffer => {
-    const text = buffer.toString();
-
-    fileLogDetailed('Read File:', text);
-
-    const html = converter.makeHtml(text);
-    fileLogDetailed('Converted to HTML', html);
-
-    const newFileName = file.replace(/md$/, 'html');
-
-    fileLog('Outputting to ', newFileName);
-
-    return fs.outputFile(newFileName, html).then(() => {
-      fileLog('Done!');
-
-      return newFileName;
-    });
-  });
-};
-
-const processFiles = files => {
-  debug('Processing Files:', files);
-  const promises = [];
-  for (let f = 0; f < files.length; f += 1) {
-    const file = files[f];
-
-    debug('Processing: ', file);
-
-    promises.push(processFile(file));
-  }
-  return Promise.all(promises);
-};
-
-// Process the glob and markdown convert each of the files.
-promiseGlob(mdGlob)
-  .then(processFiles)
-  .then(() => {
-    log('Done');
-  })
-  .catch(err => {
-    error(err);
-  });
-
-// if watch
-if (program.watch) {
-  const watcher = watch(mdGlob);
-
-  watcher.on('change', processFile);
-  watcher.on('add', processFile);
-
-  log(`Watching ${mdGlob} for changes`);
+const packtdown = new Packtdown(program);
+if(program.watch) {
+  packtdown.watch();
+} else {
+  packtdown.run();
 }
---
+++ src/packtdown.js
@@ -0,0 +1,101 @@
+const fs = require('fs-extra');
+const showdown = require('showdown');
+const watch = require('glob-watcher');
+
+
+const Logger = require('./logger.js');
+const glob = require('./promise-glob.js');
+
+class Packtdown {
+  constructor(options) {
+    this.log = new Logger('packtdown');
+
+    this.log.debug('directory: ', options.directory);
+    this.log.debug('glob: ', options.glob);
+
+    this.glob = './docs/**/*.md'; // default glob
+
+    if (options.glob) {
+      // Set the glob
+      this.log.debug('Glob Set to', options.glob);
+
+      this.glob = options.glob;
+    } else if (options.directory) {
+      this.log.debug('Directory set to', options.directory);
+      // Parse the directory into a glob
+      this.glob = `./${options.directory}/**/*.md`;
+    }
+
+    this.log.debug('Parsing files in', this.glob);
+  }
+
+  async run() {
+    // Process the glob and markdown convert each of the files.
+    try {
+      const files = await glob(this.glob);
+
+      await this.processFiles(files);
+
+      this.log.log('Done');
+    } catch (error) {
+      this.log.error('Error processing files', error.message);
+      this.log.error(error);
+    }
+  }
+
+  async watch() {
+    try {
+      await this.run();
+
+      const watcher = watch(this.glob);
+
+      watcher.on('change', Packtdown.processFile);
+      watcher.on('add', Packtdown.processFile);
+
+      this.log.log(`Watching ${this.glob} for changes`);
+    } catch (error) {
+      this.log.error('Error watching files', error.message);
+      this.log.error(error);
+    }
+  }
+
+  async processFiles(files) {
+    this.log.log('Processing files');
+    this.log.debug(files);
+
+    return Promise.all(files.map(async file => Packtdown.processFile(file)));
+  }
+
+  static async processFile(file) {
+    // Spawn tagged logs because this is a fan-out.
+    const log = new Logger(file);
+    log.log('Processing');
+
+    try {
+      const converter = new showdown.Converter();
+
+      const fileBuffer = await fs.readFile(file);
+      const fileContents = fileBuffer.toString();
+
+      log.log('Read file contents');
+      log.debug(fileContents);
+
+      const html = converter.makeHtml(fileContents);
+
+      log.log('Converted to HTML');
+      log.debug(html);
+
+      const newFileName = file.replace(/md$/, 'html');
+
+      log.log('Outputting to', newFileName);
+
+      await fs.outputFile(newFileName, html);
+      log.log('Done');
+    } catch (error) {
+      log.error('Error Processing file', error.message);
+      log.error(error);
+    }
+  }
+}
+
+module.exports = Packtdown;
